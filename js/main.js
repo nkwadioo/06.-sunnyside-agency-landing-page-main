db6c01b795d5288b49a3d9de05cd4a8da73233f8
nkwadioo <nkwadioo@gmail.com>
2021-08-09 19:01:45 +0200

    ipad tablet potrate mode

--- js/main.js
+++ js/main.js
@@ -3,4 +3,33 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('hamburger').addEventListener('click', (e) => {
         document.querySelector('header nav').classList.toggle('active')
     })
+
+    window.addEventListener('resize', (event) => {
+        // console.log(event.currentTarget.innerWidth);
+        if(event.currentTarget.innerWidth > 720) {
+            document.querySelectorAll('img[src^="./images/mobile"]').forEach( (val, key, parent) => {
+                if (val.attributes.src) {
+                    val.attributes.src.value = val.attributes.src.value.replace('mobile', 'desktop');
+                }
+            })
+        } else {
+            document.querySelectorAll('img[src^="./images/desktop"]').forEach( (val, key, parent) => {
+                if (val.attributes.src) {
+                    val.attributes.src.value = val.attributes.src.value.replace('desktop', 'mobile');
+                }
+            })
+
+        }
+    }, true);
+
+    if(window.screen.width > 720) {
+        document.querySelectorAll('img[src^="./images/mobile"]').forEach( (val, key, parent) => {
+            // console.log(val.attributes.src)
+            // console.log(val.src)
+            // find mobile and replace with desktop
+            if (val.attributes.src) {
+                val.attributes.src.value = val.attributes.src.value.replace('mobile', 'desktop');
+            }
+        })
+    }
 })
